ba7aa5b877b5b525d84e3bb2f7da82d347462f54
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-03-08 13:05:52 -0300

    Testes Java

---
+++ 0x01/calculo/OperacaoBasica.java
@@ -0,0 +1,37 @@
+public class OperacaoBasica {
+
+
+    public static double somar(double valor1, double valor2) {
+        return valor1 + valor2;
+    }
+
+    public static double subtrair(double valor1, double valor2) {
+        return valor1 - valor2;
+    }
+
+    public static double multiplicar(double valor1, double valor2) {
+        return valor1 * valor2;
+    }
+
+    public static double dividir(double valor1, double valor2) {
+        return valor1 / valor2;
+    }
+
+    public static double sqrt(double valor1) {
+        return Math.sqrt(valor1);
+    }
+
+    public static String numero_primo(double valor1) {
+
+        if (valor1 <= 1) {
+            return "Número invalido. Numero menor ou igual a 1 não é primo";
+        }
+
+        if (valor1 % 2 == 0) {
+            return "Número válido, mas o numero não é primo";
+        }else {
+            return "Numero válido, o numero é primo";
+        }
+
+    }
+}
